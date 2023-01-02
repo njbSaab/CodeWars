ee87accf14b19535dd42f043249094a64dbd4b52
Pro <Admin@Pro.local>
2023-01-02 10:33:26 +0200

    next

--- 7kyu.js
+++ 7kyu.js
@@ -1407,8 +1407,140 @@
 //   console.log(scheme('(/)'));
 
 
-const complete = str =>{
-   return
-} 
+// const complete = str =>{
+//    let strRev = [...str].reverse().join('');
+//    if(str.length === 1) return str+str;
 
-console.log(str());
+
+//    console.log(str, strRev, 'два слова');
+//    console.log(str[0] === strRev[strRev.length-1], 'сравнение 1 и -1');
+//    console.log(str === strRev, 'cравнение полной строки');
+
+
+//    if (str[str.length-1] === strRev[0].toUpperCase() && str[str.length-2] === strRev[1].toUpperCase()) return str + strRev.slice(-2);
+
+//    if (str === strRev && str[0] === strRev[strRev.length-1]) return str + strRev.slice(1);
+
+//    if (str[0] === strRev[strRev.length-1].toUpperCase()) return str + strRev[strRev.length-1];
+
+   // if (str[0] === strRev[strRev.length-1] && str[1] === strRev[strRev.length-2]) return str + strRev.slice(1);
+
+   // if(str[0] === str[0].toUpperCase()) return str+str[0]
+   // else return str+ [...str].reverse().slice(1).join('')
+// } 
+
+//  console.log(complete("xyzx")); //(baba) => aab + aa => aabaa
+//  console.log(complete("aaBB")); //(aab) => aab + aa => aabaa
+
+// console.log(complete("AaaaA")); //(aab) => aab + aa => aabaa
+
+    // if (str[str.length-1] === strRev[0].toUpperCase() && str[str.length-2] === strRev[1]) return str + strRev.slice(-2);
+
+
+
+    //* cложный палиндром
+// const complete = str => {
+//   if(str.length === 1) return str+str
+//   if(str === [...str].reverse().join('')) return str+str.slice(1)
+// //   if(str.slice(0, -1) === [...str].reverse().slice(1).reverse().join(''))
+  
+// //   return str + [...str].reverse().slice(1).join('')
+// // else {
+
+//   let res = [];
+//   let finalArr = [];
+
+//   [...str].forEach((el,i) => {
+//     if(el !== str[str.length -1 -i]) {
+//        res.push(el)
+//     };
+//   })
+//    res = res.slice(0,1)
+
+//  let final = [...str, ...res]
+//  final.forEach((el,i) => {
+//    if(el !== final[final.length -1 -i]) {
+//       finalArr.push(el)
+//    };
+//  })
+//  finalArr = finalArr.slice(0,1)
+
+//  const array = [...final, ...finalArr].join('')
+
+// return array === [...array].reverse().join('') ? array : str + [...str].reverse().slice(1).join('')
+
+// }
+
+const complete = str => {
+   // *
+   // let s = ''
+   
+   // for(let i = 0; i < str.length; i++) {
+   //   s += str[i];
+
+   //   let x = str+s.split('').reverse().join('')
+
+   //   if (x === x.split('').reverse().join('')) return x
+   // }
+
+
+   //*
+   // const strStart = str.split('')
+   // const strEnd = [strStart[0]]
+
+   // const isPalindrome = result => {
+   //     for (let i = 0; i < result.length / 2; i++) {
+   //         if (result[i] !== result[result.length - 1 - i])
+   //             return false
+   //     }
+   //     return true
+   // }
+
+   // let current = 1
+
+   // while (!isPalindrome([...strStart, ...strEnd].join(''))) {
+   //     strEnd.unshift(strStart[current])
+   //     current++
+   // }
+
+   // return [...strStart, ...strEnd].join('')
+
+//*
+//   let base = str.split("");
+//   let arr = [...base, str.charAt(0)];
+//   let i = 2;
+//   while (arr.join("") !== [...arr].reverse().join("")) {
+//     arr = [...base, ...arr.slice(0, i).reverse()];
+//     ++i;
+//   }
+//   return arr.join("");
+
+}
+console.log(complete("aab")); //aabaa
+// console.log(complete("aba"));//*ababa
+console.log(complete("Baa")); //"BaaB" 
+// console.log(complete("aaB")); //"aaBaa" 
+// console.log(complete("x")); //*xx
+// console.log(complete("aaBB")); // ['a', 'a', 'B', 'B', 'a', 'a']
+// console.log(complete("aOOa")); //*aOOaOOa
+console.log(complete("xyz")); //"xyzyx
+//  console.log(complete("AaaaA")); //*"AaaaAaaaA" 
+  console.log(complete("abab")); //ababa
+
+
+    // let res = ""
+
+   // for(let i = 0; i <= str.length-1; i++){
+   //    // console.log(str[i],str[str.length -1 -i]);
+   //    if(str[i] !== str[i],str[str.length -1]){
+   //       res += str[i]
+   //       console.log(res);
+   //    }
+      
+   // }
+   // return res
+// (a) => a + a => aa
+// (GG) => GG + G => GGG
+// (Ab) => Ab + A => AbA
+
+
